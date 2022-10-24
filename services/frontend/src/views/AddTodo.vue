78aea144528674d97c146a54c47f0664a175d0e0
Erick Wilts <erickwilts@proton.me>
2022-10-24 12:09:46 +0200

    Add some helper buttons for due_date

--- services/frontend/src/views/AddTodo.vue
+++ services/frontend/src/views/AddTodo.vue
@@ -13,6 +13,14 @@
             <div class="mb-3">
                 <label for="due_date" class="form-label">Deadline:</label>
                 <input type="datetime-local" name="due_date" v-model="form.due_date" class="form-control col-sm-6" />
+                <div class="row">
+                    <input type="button" class="btn btn-light col-md-1 m-3" value="Nu" @click="setNow" />
+                    <input type="button" class="btn btn-light col-md-1 m-3" value="+1 uur" @click="addHours(1)" />
+                    <input type="button" class="btn btn-light col-md-1 m-3" value="+2 uur" @click="addHours(2)" />
+                    <input type="button" class="btn btn-light col-md-1 m-3" value="+1 dag" @click="addDays(1)" />
+                    <input type="button" class="btn btn-light col-md-1 m-3" value="+3 dagen" @click="addDays(3)" />
+                    <input type="button" class="btn btn-light col-md-1 m-3" value="+1 week" @click="addWeeks(1)" />
+                </div>
             </div>
             <div class="mb-3">
                 <button type="submit" class="btn btn-success">Toevoegen</button>
@@ -42,7 +50,29 @@ export default {
             await this.createTodo(this.form);
             console.log("Todo created");
             this.$router.push("/todos");
-        }
-    }
+        },
+        setNow() {
+            let date = new Date();
+            this.form.due_date = new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000).toISOString().slice(0, 16);
+        },
+        addHours(hours) {
+            let date = this.form.due_date ? new Date(this.form.due_date) : new Date();
+
+            // add the number of hours specified
+            date.setHours(date.getHours() + hours);
+
+            // set the date back to the form
+            this.form.due_date = new Date(date - date.getTimezoneOffset() * 60 * 1000).toISOString().slice(0, 16);
+        },
+        addDays(days) {
+            this.addHours(days * 24);
+        },
+        addWeeks(weeks) {
+            this.addDays(weeks * 7);
+        },
+    },
+    mounted() {
+        this.setNow();
+    },
 }
 </script>
